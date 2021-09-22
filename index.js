dc0c3f31a48fbb99a62baa9990d737f90a85f969
Davyd Cristiano <53920878+davydcristiano@users.noreply.github.com>
2021-09-22 19:40:08 -0300

    Update index.js
    Correção do programa dentro da sala de aula...

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-exibirImparPar(10);
+exibirImparPar(5);
 function exibirImparPar(limite) {
     for (var i = 0; i <= limite; i++) {
-        if (i % 2 === 0)
+        if (i % 2 === 0 && i != 0)
         console.log(i,"PAR - Classificado!");
-        else
+        else if (i % 2 === 1)
         console.log(i,"IMPAR - Desclassificado!");
+        else
+        console.log("número é igual á 0");
     }
 }
